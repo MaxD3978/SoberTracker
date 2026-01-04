bc426f7e8f7e40385a67005a6d754526ea585250
MaxD3978 <dundukow.max@gmail.com>
2026-01-04 19:19:15 +0100

    Update sw.js

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,1 +1,19 @@
+const CACHE_NAME = "soberfeb-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/app.js",
+  "/pwa/manifest.webmanifest",
+  "/pwa/icon-192.png",
+  "/pwa/icon-512.png"
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
